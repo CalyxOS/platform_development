2bc4ca365e3417629f0ed9f28432a4817febbb22
Michael Bestas <mkbestas@gmail.com>
2023-03-28 00:04:34 +0300

    process_prop_template: Handle CALYXOS_VERSION
    Change-Id: I5195db9391595cf65013eb2f342dc76360512eef

--- build/tools/sdk-preprocess-files.mk
+++ build/tools/sdk-preprocess-files.mk
@@ -39,6 +39,7 @@ define process_prop_template
 @echo Generate $@
 $(hide) mkdir -p $(dir $@)
 $(hide) sed \
+	-e 's/$${CALYXOS_VERSION}/$(CALYXOS_VERSION)/' \
 	-e 's/$${PLATFORM_VERSION}/$(PLATFORM_VERSION)/' \
 	-e 's/$${PLATFORM_SDK_VERSION}/$(subst ",,$(PLATFORM_SDK_VERSION_FULL))/' \
 	-e 's/$${PLATFORM_SDK_EXTENSION_VERSION}/$(PLATFORM_SDK_EXTENSION_VERSION)/' \
